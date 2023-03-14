e73069fb6ab8ea07cc1267d2feb95755f8c2c301
licask8 <elielson.elrick@gmail.com>
2023-03-14 17:22:08 -0300

    iniciando estrutura home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,8 @@
-import { Container } from './styles'
+import { Container, Logo, ContainerInfo, TextInfo, TextDescription, ContainerSearch, ContentSearch, DogsImage } from './styles'
+import logo from '../../assets/images/logo.png'
+import dogs from '../../assets/images/dogs.png'
+import search from '../../assets/icons/search.svg'
+import arrowBottom from "../../assets/icons/chevron-bottom.svg"
 
 export function Home() {
   function handleSearchPets() {
@@ -13,5 +17,44 @@ export function Home() {
     // TO DO
   }
 
-  return <Container>{/* COMECE POR AQUI */}</Container>
+  return (
+    <Container>
+      <ContainerInfo>
+        <Logo>
+          <img src={logo} alt="logo" />
+        
+        </Logo>
+
+        <TextInfo> Leve
+           a felicidade 
+           para o seu lar</TextInfo>
+
+        <TextDescription> Encontre o animal de estimação ideal
+           para seu estilo de vida! </TextDescription>
+
+      </ContainerInfo>
+
+      <ContainerSearch>
+        <DogsImage><img src={dogs} alt="" /> </DogsImage>
+
+        <ContentSearch>
+          Busque um amigo:
+
+          <button>
+            PE
+            <img src={arrowBottom} alt="seta para baixo" />
+          </button>
+
+          <input type="text" placeholder='Recife' />
+
+          <button>
+            <img src={search} alt="ícone de lupa" />
+          </button>
+         
+
+        </ContentSearch>
+
+      </ContainerSearch>
+      
+    </Container>)
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,3 +1,123 @@
 import styled from 'styled-components'
 
-export const Container = styled.div``
+export const Container = styled.div`
+ display: flex;
+ flex-direction: row;
+ background-color: #F15156;
+ padding-left: 113px;
+ height:100vh;
+ gap: 136px;
+`;
+
+ export const ContainerInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 487px;
+  height: 100%;
+  padding-top: 75px;
+  
+ 
+
+`;
+
+
+export const Logo = styled.div`
+ width: 215px;
+ height: 56px;
+
+`;
+
+export const TextInfo = styled.text`
+ font-size: 72px;
+ font-weight: 800;
+ line-height: 64.8px;
+`;
+
+export const TextDescription = styled.text`
+ margin-bottom: 100px;
+ font-size: 24px;
+ font-weight: 600;
+ line-height: 34px;
+ color: #ffffff
+`;
+
+
+export const ContainerSearch = styled.div`
+ display: flex;
+ flex-direction: column;
+ 
+ width: 487px;
+ height: 100%;
+
+`;
+
+export const DogsImage = styled.image`
+ display: flex;
+ margin-top: 120px;
+  
+`;
+
+export const ContentSearch = styled.div`
+ display: flex;
+ margin-top: 126px;
+ width: 600px;
+ align-items: center;
+ gap: 23px;
+
+ input {
+    width: 203px;
+    height: 60px;
+    font-size: 16px;
+    font-weight: bold;
+    background-color: #E44449;
+    padding: 21px 16px;
+    border-radius: 15px;
+    border: 1px solid #E44449;
+    color: #fff;
+
+    outline: none;
+
+    &::placeholder {
+      color: #f2f2f2;
+      text-align: center;
+    }
+  }
+
+  button:first-child {
+    background-color: transparent;
+    border: 1px solid #ffffff;
+    color: white;
+
+    img {
+        width: 8px;
+        height: 4px;
+        margin-left: 8px;;
+    }
+  }
+
+  button {
+    width: 60px;
+    height: 60px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    background-color: #f4d35e;
+    border: none;
+    border-radius: 20px;
+    transition: filter 0.2s;
+
+    img {
+      width: 22px;
+    }
+
+    :hover {
+      filter: brightness(0.9);
+    }
+  }
+  
+`;
+
+
+
